d29f58f4be9a1c73b5fb719699ad343995ded6ee
Philipe Emanuel <xphilipsilva@gmail.com>
2026-08-24 17:23:31 -0300

    Update

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 // dos arquivos principais.
 // ==========================================================================
 
-const NOME_DO_CACHE = "financas-familia-v22";
+const NOME_DO_CACHE = "financas-familia-v24";
 
 // Arquivos essenciais, guardados localmente no primeiro acesso
 const ARQUIVOS_ESSENCIAIS = [
